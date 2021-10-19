737e387f6b769c7fc8a126b610e65d1f2e762016
Georg Kallidis <gk@apache.org>
2021-10-19 17:12:49 +0200

    before writing allow to resolve entities with parser, activate by default, requires:pip install bs4

--- oaiharvest/stores/directory_store.py
+++ oaiharvest/stores/directory_store.py
@@ -10,19 +10,31 @@ from six.moves.urllib import parse as urllib
 
 from oaiharvest.record import Record
 
+from bs4 import BeautifulSoup
+#from html.parser import HTMLParser
 
 class DirectoryRecordStore(object):
-    def __init__(self, directory, createSubDirs=False):
+    def __init__(self, directory, createSubDirs=False, resolveEntities=True):
         self.directory = directory
         self.createSubDirs = createSubDirs
+        self.resolveEntities = resolveEntities
         self.logger = logging.getLogger(__name__).getChild(self.__class__.__name__)
 
     def write(self, record: Record, metadataPrefix: str):
         fp = self._get_output_filepath(record.header, metadataPrefix)
         self._ensure_dir_exists(fp)
         self.logger.debug("Writing to file {0}".format(fp))
-        with codecs.open(fp, "w", encoding="utf-8") as fh:
-            fh.write(record.metadata)
+        if (record.metadata is None):
+            self.logger.info("*** No metadata for {0}.{1}".format(record.header.identifier(), metadataPrefix) )
+            return
+        if self.resolveEntities:
+            result = BeautifulSoup(str(record.metadata),"html.parser")
+            result = str(result)
+        else:
+            result = record.metadata
+            
+        with open( fp, 'w', encoding="utf8", newline='\n') as fh: 
+            fh.write( result )
 
     def delete(self, record: Record, metadataPrefix: str):
         fp = self._get_output_filepath(record.header, metadataPrefix)
